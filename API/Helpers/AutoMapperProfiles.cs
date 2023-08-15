51196f7d1a24430379b95bfeb8b9b6560a0930bd
besika94 <besika94@gmail.com>
2023-08-15 15:35:34 +0100

    update user info, cache info

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -16,6 +16,7 @@ namespace API.Helpers
                                          x => x.IsMain).Url))
                 .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotoDto>();
+            CreateMap<MemberUpdateDto, AppUser>();
         }
     }
 }
---
+++ client/src/app/guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,11 @@
+import { CanDeactivateFn } from '@angular/router';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+
+export const preventUnsavedChangesGuard: CanDeactivateFn<
+  MemberEditComponent
+> = (component) => {
+  if (component.editForm?.dirty) {
+    return confirm('You have unsaved changes, do you want to proceed ?');
+  }
+  return true;
+};
